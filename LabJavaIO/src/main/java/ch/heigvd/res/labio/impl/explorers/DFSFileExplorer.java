e33f82d8b2d014c3a0eae62eb64dea3e311cff86
ncdm-stldr <ncdm.stldr@gmail.com>
2019-03-13 23:24:08 +0100

    validating test DFSFileExplorerTest

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/explorers/DFSFileExplorer.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/explorers/DFSFileExplorer.java
@@ -2,21 +2,37 @@ package ch.heigvd.res.labio.impl.explorers;
 
 import ch.heigvd.res.labio.interfaces.IFileExplorer;
 import ch.heigvd.res.labio.interfaces.IFileVisitor;
+
 import java.io.File;
+import java.util.LinkedList;
 
 /**
  * This implementation of the IFileExplorer interface performs a depth-first
  * exploration of the file system and invokes the visitor for every encountered
  * node (file and directory). When the explorer reaches a directory, it visits all
  * files in the directory and then moves into the subdirectories.
- * 
+ *
  * @author Olivier Liechti
  */
 public class DFSFileExplorer implements IFileExplorer {
 
   @Override
   public void explore(File rootDirectory, IFileVisitor vistor) {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+
+    vistor.visit(rootDirectory);
+    if(!rootDirectory.isDirectory()) return;
+
+    LinkedList<File> directories = new LinkedList<>();
+    LinkedList<File> files = new LinkedList<>();
+    for (final File fileEntry : rootDirectory.listFiles()) {
+      if (fileEntry.isDirectory()) directories.add(fileEntry);
+      else files.add(fileEntry);
+    }
+    
+    for (File f : files) vistor.visit(f);
+    for (File dir : directories) explore(dir, vistor);
+    return;
   }
 
 }
+
